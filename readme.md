839f472a8be637b4994f738b47f7eeca58c9e5e6
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2024-12-10 10:47:10 -0500

    init and readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+# CryptoPunks Timeseries and Aggregations Example Repo
+
+This repository provides a basic example of using timeseries and aggregations with The Graph. It demonstrates how to set up a subgraph that tracks various events related to CryptoPunks, such as transfers, bids, and sales, and aggregates this data over time.
+
+## Helpful Links
+
+- **Video Walkthrough:**  
+  [Watch on YouTube](https://youtu.be/V7yW8zB4EuY)
+
+- **Timeseries and Aggregations Readme:**  
+  [Read the Documentation](https://github.com/graphprotocol/graph-node/blob/master/docs/aggregations.md)
+
+- **Official Aggregations Example Repo:**  
+  [Explore the Repository](https://github.com/graphprotocol/graph-tooling/tree/7faa3098b2e6c61f09fc81b8b2d333e66b0080d1/examples/aggregations)
--- src/crypto-punks-market.ts
+++ src/crypto-punks-market.ts
@@ -1,140 +1,147 @@
 import {
-  Assign as AssignEvent,
-  Transfer as TransferEvent,
-  PunkTransfer as PunkTransferEvent,
-  PunkOffered as PunkOfferedEvent,
-  PunkBidEntered as PunkBidEnteredEvent,
-  PunkBidWithdrawn as PunkBidWithdrawnEvent,
-  PunkBought as PunkBoughtEvent,
-  PunkNoLongerForSale as PunkNoLongerForSaleEvent
-} from "../generated/CryptoPunksMarket/CryptoPunksMarket"
+	Assign as AssignEvent,
+	Transfer as TransferEvent,
+	PunkTransfer as PunkTransferEvent,
+	PunkOffered as PunkOfferedEvent,
+	PunkBidEntered as PunkBidEnteredEvent,
+	PunkBidWithdrawn as PunkBidWithdrawnEvent,
+	PunkBought as PunkBoughtEvent,
+	PunkNoLongerForSale as PunkNoLongerForSaleEvent,
+} from "../generated/CryptoPunksMarket/CryptoPunksMarket";
 import {
-  Assign,
-  Transfer,
-  PunkTransfer,
-  PunkOffered,
-  PunkBidEntered,
-  PunkBidWithdrawn,
-  PunkBought,
-  PunkNoLongerForSale
-} from "../generated/schema"
+	Assign,
+	Transfer,
+	PunkTransfer,
+	PunkOffered,
+	PunkBidEntered,
+	PunkBidWithdrawn,
+	PunkBought,
+	PunkNoLongerForSale,
+	PunkBoughtData,
+} from "../generated/schema";
+import { crypto } from "@graphprotocol/graph-ts";
 
 export function handleAssign(event: AssignEvent): void {
-  let entity = new Assign(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.to = event.params.to
-  entity.punkIndex = event.params.punkIndex
+	let entity = new Assign(
+		event.transaction.hash.concatI32(event.logIndex.toI32())
+	);
+	entity.to = event.params.to;
+	entity.punkIndex = event.params.punkIndex;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+	entity.blockNumber = event.block.number;
+	entity.blockTimestamp = event.block.timestamp;
+	entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+	entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
+	let entity = new Transfer(
+		event.transaction.hash.concatI32(event.logIndex.toI32())
+	);
+	entity.from = event.params.from;
+	entity.to = event.params.to;
+	entity.value = event.params.value;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+	entity.blockNumber = event.block.number;
+	entity.blockTimestamp = event.block.timestamp;
+	entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+	entity.save();
 }
 
 export function handlePunkTransfer(event: PunkTransferEvent): void {
-  let entity = new PunkTransfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.punkIndex = event.params.punkIndex
+	let entity = new PunkTransfer(
+		event.transaction.hash.concatI32(event.logIndex.toI32())
+	);
+	entity.from = event.params.from;
+	entity.to = event.params.to;
+	entity.punkIndex = event.params.punkIndex;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+	entity.blockNumber = event.block.number;
+	entity.blockTimestamp = event.block.timestamp;
+	entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+	entity.save();
 }
 
 export function handlePunkOffered(event: PunkOfferedEvent): void {
-  let entity = new PunkOffered(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.punkIndex = event.params.punkIndex
-  entity.minValue = event.params.minValue
-  entity.toAddress = event.params.toAddress
+	let entity = new PunkOffered(
+		event.transaction.hash.concatI32(event.logIndex.toI32())
+	);
+	entity.punkIndex = event.params.punkIndex;
+	entity.minValue = event.params.minValue;
+	entity.toAddress = event.params.toAddress;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+	entity.blockNumber = event.block.number;
+	entity.blockTimestamp = event.block.timestamp;
+	entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+	entity.save();
 }
 
 export function handlePunkBidEntered(event: PunkBidEnteredEvent): void {
-  let entity = new PunkBidEntered(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.punkIndex = event.params.punkIndex
-  entity.value = event.params.value
-  entity.fromAddress = event.params.fromAddress
+	let entity = new PunkBidEntered(
+		event.transaction.hash.concatI32(event.logIndex.toI32())
+	);
+	entity.punkIndex = event.params.punkIndex;
+	entity.value = event.params.value;
+	entity.fromAddress = event.params.fromAddress;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+	entity.blockNumber = event.block.number;
+	entity.blockTimestamp = event.block.timestamp;
+	entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+	entity.save();
 }
 
 export function handlePunkBidWithdrawn(event: PunkBidWithdrawnEvent): void {
-  let entity = new PunkBidWithdrawn(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.punkIndex = event.params.punkIndex
-  entity.value = event.params.value
-  entity.fromAddress = event.params.fromAddress
+	let entity = new PunkBidWithdrawn(
+		event.transaction.hash.concatI32(event.logIndex.toI32())
+	);
+	entity.punkIndex = event.params.punkIndex;
+	entity.value = event.params.value;
+	entity.fromAddress = event.params.fromAddress;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+	entity.blockNumber = event.block.number;
+	entity.blockTimestamp = event.block.timestamp;
+	entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+	entity.save();
 }
 
 export function handlePunkBought(event: PunkBoughtEvent): void {
-  let entity = new PunkBought(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.punkIndex = event.params.punkIndex
-  entity.value = event.params.value
-  entity.fromAddress = event.params.fromAddress
-  entity.toAddress = event.params.toAddress
+	let entity = new PunkBought(
+		event.transaction.hash.concatI32(event.logIndex.toI32())
+	);
+	entity.punkIndex = event.params.punkIndex;
+	entity.value = event.params.value;
+	entity.fromAddress = event.params.fromAddress;
+	entity.toAddress = event.params.toAddress;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+	entity.blockNumber = event.block.number;
+	entity.blockTimestamp = event.block.timestamp;
+	entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+	entity.save();
+
+	let dataEntity = new PunkBoughtData("auto");
+	dataEntity.punkIndex = event.params.punkIndex;
+	dataEntity.value = event.params.value;
+	dataEntity.save();
 }
 
 export function handlePunkNoLongerForSale(
-  event: PunkNoLongerForSaleEvent
+	event: PunkNoLongerForSaleEvent
 ): void {
-  let entity = new PunkNoLongerForSale(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.punkIndex = event.params.punkIndex
+	let entity = new PunkNoLongerForSale(
+		event.transaction.hash.concatI32(event.logIndex.toI32())
+	);
+	entity.punkIndex = event.params.punkIndex;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+	entity.blockNumber = event.block.number;
+	entity.blockTimestamp = event.block.timestamp;
+	entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+	entity.save();
 }
